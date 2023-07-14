9794c83e852d844b6932987a076a2705cfe87bc9
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2023-07-14 23:07:35 +0300

    Добавьте файлы проекта.

---
+++ WebApplication7.2.Tests/Controllers/MapsControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using WebApplication7._2;
+using WebApplication7._2.Controllers;
+namespace WebApplication7._2.Tests.Controllers
+{
+    [TestClass]
+    public class MapsControllerTest
+    {
+        [TestMethod]
+        public void ViewMaps()
+        {
+            // Arrange
+            MapsController controller = new MapsController();
+            // Act
+            ViewResult result = controller.ViewMaps() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ WebApplication7.2/Controllers/MapsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication7._2.Controllers
+{
+    public class MapsController : Controller
+    {
+        // GET: Maps
+        public ActionResult ViewMaps()
+        {
+            // Add action logic here
+            return View();
+        }
+    }
+}
